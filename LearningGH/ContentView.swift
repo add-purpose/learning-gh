b169ec26474d73f94bb4c5fc2e604424a0b497df
add-purpose <124504794+add-purpose@users.noreply.github.com>
2023-02-05 13:23:31 +0000

    Update ContentView to Hola

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, sentient beings!")
+            Text("Hola, sentient beings!")
         }
         .padding()
     }
