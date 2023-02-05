a79e85d8dbcc155cce74fb9c5ec20410fad0d311
Mikey <mikey@addpurpose.co.uk>
2023-02-05 13:01:57 +0000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Michael Singh on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
